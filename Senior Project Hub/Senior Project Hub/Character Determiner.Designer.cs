ca95e3dc78d3a20bbb31c98895e4f25b79b4a53c
Zach879 <31457779+Zach879@users.noreply.github.com>
2025-04-29 13:27:28 -0400

    Add files via upload

---
+++ Senior Project Hub/Senior Project Hub/Character Determiner.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Senior_Project_Hub
+{
+    partial class frmSeniorProjectHub
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnSelectImage = new System.Windows.Forms.Button();
+            this.picMain = new System.Windows.Forms.PictureBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.psbStatus = new System.Windows.Forms.ProgressBar();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.tmrProgressTracker = new System.Windows.Forms.Timer(this.components);
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(475, 371);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 23);
+            this.btnLeft.TabIndex = 0;
+            this.btnLeft.Text = "< Left";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Visible = false;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(729, 371);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(75, 23);
+            this.btnRight.TabIndex = 0;
+            this.btnRight.Text = "Right >";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Visible = false;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnSelectImage
+            // 
+            this.btnSelectImage.Location = new System.Drawing.Point(556, 363);
+            this.btnSelectImage.Name = "btnSelectImage";
+            this.btnSelectImage.Size = new System.Drawing.Size(167, 38);
+            this.btnSelectImage.TabIndex = 0;
+            this.btnSelectImage.Text = "Select Image (PNG)";
+            this.btnSelectImage.UseVisualStyleBackColor = true;
+            this.btnSelectImage.Click += new System.EventHandler(this.btnSelectImage_Click);
+            // 
+            // picMain
+            // 
+            this.picMain.Location = new System.Drawing.Point(84, 51);
+            this.picMain.Name = "picMain";
+            this.picMain.Size = new System.Drawing.Size(1110, 158);
+            this.picMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMain.TabIndex = 1;
+            this.picMain.TabStop = false;
+            this.picMain.Visible = false;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(403, 235);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(472, 20);
+            this.txtResult.TabIndex = 2;
+            this.txtResult.Visible = false;
+            this.txtResult.TextChanged += new System.EventHandler(this.txtResult_TextChanged);
+            // 
+            // psbStatus
+            // 
+            this.psbStatus.Location = new System.Drawing.Point(475, 291);
+            this.psbStatus.Name = "psbStatus";
+            this.psbStatus.Size = new System.Drawing.Size(329, 23);
+            this.psbStatus.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.psbStatus.TabIndex = 3;
+            this.psbStatus.Visible = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(584, 320);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(110, 37);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Visible = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(475, 272);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Visible = false;
+            // 
+            // tmrProgressTracker
+            // 
+            this.tmrProgressTracker.Tick += new System.EventHandler(this.tmrProgressTracker_Tick);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(590, 407);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(98, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete Line";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Visible = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // frmSeniorProjectHub
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1278, 435);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.psbStatus);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.picMain);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnSelectImage);
+            this.Controls.Add(this.btnLeft);
+            this.Name = "frmSeniorProjectHub";
+            this.Text = "Character Determiner";
+            this.Load += new System.EventHandler(this.frmSeniorProjectHub_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnSelectImage;
+        private System.Windows.Forms.PictureBox picMain;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.ProgressBar psbStatus;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Timer tmrProgressTracker;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Senior Project Hub/Senior Project Hub/Character Determiner.cs
@@ -0,0 +1,204 @@
+﻿//Zachary Reese ~ Last Edit: 4/8/2021 ~ Hub for Senior Project. User inputs png image, which is then given to OCR Scanner for evaluation (with live updates on progress). After OCR Scanner gives results, the user is allowed to edit the results including editing text and deleting lines. The user also has the option to restart or save the results to a text file.
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Senior_Project_Hub
+{
+    public partial class frmSeniorProjectHub : Form
+    {
+        string currentDirectory = Directory.GetCurrentDirectory();
+        Image mainImage = null;
+        int indexAccumulator = 0;
+        public frmSeniorProjectHub()
+        {
+            InitializeComponent();
+        }
+        int intIndex = 0;
+        List<Image> lstBoundingBoxes = new List<Image> {}; //Variable initialization
+        List<string> lstResults = new List<string> {};
+        private void btnSelectImage_Click(object sender, EventArgs e)
+        {
+            if (btnSelectImage.Text == "Select Image (PNG)") //Allows user to select image
+            { //Start if
+                openFileDialog1.InitialDirectory = currentDirectory;
+                openFileDialog1.Filter = "Image Files(*.png)|*.png"; //Setting openfiledialog properties
+                openFileDialog1.Title = "Select Paragraph Image (png)";
+                if (openFileDialog1.ShowDialog() == DialogResult.OK) //user hasn't hit cancel
+                { //start nested if
+                    mainImage = Image.FromFile(openFileDialog1.FileName);
+                    mainImage.Save(currentDirectory + "\\Data\\Original.png");
+                    picMain.Image = mainImage;
+                    picMain.Visible = true;
+                    btnStart.Visible = true;
+                } //end nested if
+            } else{
+                //saves results to text file
+                saveFileDialog1.InitialDirectory = currentDirectory;
+                saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog1.Title = "Select Location to Store Text File";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                { //start nested if
+                    File.WriteAllLines(saveFileDialog1.FileName, lstResults);
+                    restartProperties();
+                } //end nested if
+
+            } //end if
+        } //end class
+
+        private void restartProperties() //Changes form object properties when the program restarts process
+        {
+            txtResult.Visible = false;
+            btnLeft.Visible = false;
+            btnRight.Visible = false;
+            btnDelete.Visible = false;
+            btnStart.Text = "Start";
+            btnSelectImage.Text = "Select Image (PNG)";
+            picMain.Image.Dispose();
+            picMain.Visible = false;
+            btnStart.Visible = false;
+            txtResult.Clear();
+            lstBoundingBoxes.Clear();
+            lstResults.Clear();
+            intIndex = 0;
+        } //end void
+
+        private void startProcess()
+        {
+            btnSelectImage.Visible = false;
+            btnSelectImage.Text = "Save to Text File";
+            btnStart.Visible = false;
+            btnStart.Text = "Restart";
+            psbStatus.Visible = true;
+            lblStatus.Visible = true;
+            using (StreamWriter writer = new StreamWriter(currentDirectory + "\\Data\\Status.txt"))
+            {
+                writer.WriteLine("Initializing Process");
+                writer.WriteLine(Convert.ToString(indexAccumulator));
+            }
+
+            Process proc = new Process();
+            proc.StartInfo.UseShellExecute = false;
+            proc.StartInfo.FileName = "C:\\Users\\zcrzc\\AppData\\Local\\Programs\\Python\\Python38\\python.exe"; //BCTC
+                                                                                                                  //proc.StartInfo.FileName = "C:\\Users\\zcrzc\\AppData\\Local\\Programs\\Python\\Python38\\python.exe"; //HOME
+                                                                                                                  //proc.StartInfo.FileName = "C:\\Users\zcrzc\\AppData\Local\\Programs\\Python\\Python38\\python.exe"; //LAPTOP
+                                                                                                                  //proc.StartInfo.FileName = "C:\\Users\\z_reese\\AppData\\Local\\Programs\\Python\\Python38\\python.exe"; //BCTC
+            proc.StartInfo.Arguments = "\"" + currentDirectory + "\\Data\\OCR Scanner.py\"";
+            proc.StartInfo.CreateNoWindow = true;
+            proc.Start();
+            tmrProgressTracker.Enabled = true;
+            txtResult.Visible = false;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        { //Starts OCR Scanner
+            if (btnStart.Text == "Start"){
+
+                startProcess();
+
+            } else{ //restarts program process for another instance
+                if (MessageBox.Show("Are you sure you want to restart the process?", "Delete Line?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    restartProperties();
+            } //end if
+        } //end void
+        private void tmrProgressTracker_Tick(object sender, EventArgs e)
+        {
+            try //checks for the status of OCR Scanner
+            {
+                var lines = File.ReadAllLines(currentDirectory + "\\Data\\Status.txt");
+                if (lines[1] != "-1") {
+                    lblStatus.Text = Convert.ToString(lines[0]); //updates scan status on form
+                    psbStatus.Value = Convert.ToInt32(lines[1]);
+                    if (lines[1] == "100") //OCR Scanner has completed its scan
+                    {
+                        tmrProgressTracker.Enabled = false;
+                        var line = File.ReadAllLines(currentDirectory + "\\Data\\Results.txt");
+                        foreach (var result in line)
+                        {
+                            lstResults.Add(Convert.ToString(result));
+                        } //end foreach
+                        int tempacc = 0;
+                        for (int acc = 0; acc < lstResults.Count; acc += 1)
+                        {
+                            lstBoundingBoxes.Add(Image.FromFile(currentDirectory + "\\Data\\BoundingBoxes\\BoundingBoxImage" + Convert.ToString(acc + indexAccumulator) + ".png"));
+                            tempacc += 1;
+                        } //end for
+                        indexAccumulator += tempacc;
+                        picMain.Image = lstBoundingBoxes[0];
+                        txtResult.Text = lstResults[0];
+                        txtResult.Visible = true;
+                        btnSelectImage.Visible = true;
+                        lblStatus.Visible = false;
+                        btnLeft.Visible = true;
+                        btnRight.Visible = true;
+                        btnDelete.Visible = true;
+                        btnStart.Visible = true;
+                        psbStatus.Visible = false;
+                        psbStatus.Value = 0;
+                    } //end nested if
+                } else {
+                    psbStatus.Value = 0;
+                    lstBoundingBoxes.Clear();
+                    lstResults.Clear();
+                    startProcess();
+                } //end if
+            } catch (Exception ex) { } //end try
+        } //end void
+
+        private void btnLeft_Click(object sender, EventArgs e) //Moves slide image one left
+        {
+            intIndex -= 1;
+            if (intIndex == -1)
+                intIndex = lstResults.Count - 1;
+            picMain.Image = lstBoundingBoxes[intIndex];
+            txtResult.Text = lstResults[intIndex];
+        } //end void
+
+        private void btnRight_Click(object sender, EventArgs e) //Moves slide image on right
+        {
+            intIndex += 1;
+            if (intIndex == lstResults.Count)
+                intIndex = 0;
+            picMain.Image = lstBoundingBoxes[intIndex];
+            txtResult.Text = lstResults[intIndex];
+        } //end void
+
+        private void txtResult_TextChanged(object sender, EventArgs e) //Autosaves text changes
+        {
+            lstResults[intIndex] = txtResult.Text;
+        } //end void
+
+        private void btnDelete_Click(object sender, EventArgs e) //Deletes a line and its related content
+        {
+            if (MessageBox.Show("Are you sure you want to delete line #" + Convert.ToString(intIndex + 1) + "?", "Delete Line?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            { //start if
+                if (lstResults.Count - 1 == 0) //start nested if
+                {
+                    txtResult.Text = "There are no lines remaining... automatically restarting the process.";
+                    restartProperties();
+                } else {
+                    lstBoundingBoxes.RemoveAt(intIndex);
+                    lstResults.RemoveAt(intIndex);
+                    intIndex -= 1;
+                    if (intIndex == -1)
+                        intIndex = 0;
+                    picMain.Image = lstBoundingBoxes[intIndex];
+                    txtResult.Text = lstResults[intIndex];
+                } //end nested if
+            } //end if
+        } //end void
+
+        private void frmSeniorProjectHub_Load(object sender, EventArgs e)
+        {
+            string[] filePaths = Directory.GetFiles(currentDirectory + "\\Data\\BoundingBoxes\\");
+            foreach (string filePath in filePaths)
+            {
+                File.SetAttributes(filePath, FileAttributes.Normal);
+                File.Delete(filePath);
+            } //end foreach
+        } //end void
+    } //end class
+} //end namespace
